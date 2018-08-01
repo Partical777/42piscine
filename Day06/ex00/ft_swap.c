b876f128f4a157142e5435ef23de7f3fa39d3464
Partical <z25993021@gmail.com>
2018-07-31 17:24:14 -0700

    add sh command

--- Day06/ex00/ft_swap.c
+++ Day06/ex00/ft_swap.c
@@ -1,23 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: wweng <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/31 12:48:43 by wweng             #+#    #+#             */
-/*   Updated: 2018/07/31 12:52:07 by wweng            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+ 
+void ft_swap(int *a, int *b);
+int main()
+{ 
+    int a = 2;
+    int b = 6;
+    ft_swap(&a, &b);
+    return 0;
+}
 
-void	ft_swap(int *a, int *b){
-	int *x;
-	int *y
-	int temp;
-
-	x = *&a;
-	y = *&b;
-	temp = *y;
-	*y = *x;
-	*x = temp;
+void ft_swap(int *a, int *b){
+  int *x, *y, temp;
+ 
+   printf("Before Swapping\nx = %d\ny = %d\n", *a, *b);
+ 
+   x = *&a;
+   y = *&b;
+ 
+   temp = *y;
+   *y = *x;
+   *x = temp;
+ 
+   printf("After Swapping\nx = %d\ny = %d\n", *a, *b);
 }
